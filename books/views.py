ab575cb1e4fba955e1d64e646355442216c3273c
Viktoriyapp <todorova.p.viktoria@gmail.com>
2026-03-22 02:18:13 +0200

    Added simple view

--- books/views.py
+++ books/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.views import APIView
 
 
 # Create your views here.
+
+class HomeView(APIView):
+    def get(self, request):
+        return HttpResponse({"text": "helloo!"}, content_type="application/json")
