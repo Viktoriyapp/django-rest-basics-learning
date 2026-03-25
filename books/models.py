e1efc6d5339c97675e546d44a53528c942da4b41
Viktoriyapp <todorova.p.viktoria@gmail.com>
2026-03-25 04:30:17 +0200

    Added models and serializer

--- books/models.py
+++ books/models.py
@@ -6,4 +6,29 @@ class Book(models.Model):
     title = models.CharField(max_length=20)
     pages = models.IntegerField(default=0)
     description = models.TextField(max_length=100, default="")
-    author = models.CharField(max_length=20)
+    authors = models.ManyToManyField(to="Author", related_name="books")
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    established_year = models.PositiveIntegerField()
+    location = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Review(models.Model):
+    description = models.TextField(max_length=100, default="")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
--- books/serializers.py
+++ books/serializers.py
@@ -1,35 +1,87 @@
 from rest_framework import serializers
 
-from books.models import Book
-
-# class BookSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Book
-#         fields = "__all__"
+from books.models import Book, Publisher, Author, Review
 
 
-class BookSerializer(serializers.Serializer):
-    title = serializers.CharField(
-        max_length=200,
-    )
-    pages = serializers.IntegerField(
-        default=0,
-    )
-    description = serializers.CharField(
-        max_length=100,
-        default="",
-    )
-    author = serializers.CharField(
-        max_length=100,
-    )
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = "__all__"
+
+
+class PublisherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Publisher
+        fields = "__all__"
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'books']
+        """
+        {
+            "name": "viki",
+            "books": [
+                1,
+                2,
+                3,
+            ]
+        }
+
+        books = BookSerializer(many=True, read_only=True) ->
+        {
+            "name": "viki",
+            "books": [
+                {
+                    "title": "Book 1",
+                    "decsription": "..."
+                },
+                {...},
+                {...},
+            ]
+        }
+        """
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = "__all__"
 
     def create(self, validated_data):
-        return Book.objects.create(**validated_data)
+        return Review.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.pages = validated_data.get('pages', instance.pages)
         instance.description = validated_data.get('description', instance.description)
-        instance.author = validated_data.get('author', instance.author)
+        instance.book = validated_data.get('book', instance.book)
         instance.save()
         return instance
+
+
+# class BookSerializer(serializers.Serializer):
+#     title = serializers.CharField(
+#         max_length=200,
+#     )
+#     pages = serializers.IntegerField(
+#         default=0,
+#     )
+#     description = serializers.CharField(
+#         max_length=100,
+#         default="",
+#     )
+#     author = serializers.CharField(
+#         max_length=100,
+#     )
+#
+#     def create(self, validated_data):
+#         return Book.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.pages = validated_data.get('pages', instance.pages)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.save()
+#         return instance
